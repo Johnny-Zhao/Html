e916dfdc6a0d235fd9328d3cfc6dd64daa277bb0
Johnny-Zhao <897192572@qq.com>
2017-06-24 18:51:46 +0800

    first_part

---
+++ 觉唯网页/scripts/float.js
@@ -0,0 +1,18 @@
+// window.onload= function(){
+// 	var id = document.getElementById("content_image");
+// 	var imgs = document.getElementsByTagName("img");
+// 	var ps = document.getElementsByTagName("p");
+// 	var timer = null;
+// 	for(var i = 0; i<=imgs.length;i++){
+// 		imgs[i].onmouseover = function(){
+// 			clearInterval(timer);
+// 			timer = setInterval(function(){
+// 			//获取滚动条距离顶部的高度
+// 			var osTop = this.offsetHeight;
+// 			osTop = osTop + 5+ "px";
+// 			this.style.
+// 			clearInterval(timer);
+// 		},30);
+// 		}
+// 	}
+// }
